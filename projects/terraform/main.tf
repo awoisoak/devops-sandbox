a95639b0fd944104b580da8401af8e9bbf83d70b
awoisoak <awoisoak@gmail.com>
2022-10-26 23:52:01 +0900

    Print public DNS generated and the approximated cost of the architecture

--- projects/terraform/main.tf
+++ projects/terraform/main.tf
@@ -12,13 +12,16 @@ resource "aws_key_pair" "photoshop_key" {
   public_key = file("/Users/awo/.ssh/aws_id_rsa.pub")
 }
 
-#Create an Elastic IP to make sure the webserver gets a fixed ip
+# Create an Elastic IP to make sure the webserver gets a fixed ip
 resource "aws_eip" "web_server_ip" {
   vpc      = true
   instance = aws_instance.web_server.id
 
+  # Print public DNS generated and approximated cost of architecture
   provisioner "local-exec" {
-    command = "echo 'Web server public DNS: ' ${self.public_dns}"
-
+    command = <<EOT
+        echo ${self.public_dns} ;
+        terraform state pull | curl -s -X POST -H 'Content-Type: application/json' -d @- https://cost.modules.tf/
+    EOT
   }
 }
