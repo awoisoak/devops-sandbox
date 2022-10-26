b441affd7641646121a3a9b64a7a49f82eb04a99
awoisoak <awoisoak@gmail.com>
2022-10-26 23:50:55 +0900

    Add configuration to run Terraform against LocalStack

--- projects/terraform/provider.tf
+++ projects/terraform/provider.tf
@@ -9,4 +9,19 @@ terraform {
 
 provider "aws" {
   region = var.region
+
+  ##########################################################
+  # Extra configuration to emulate AWS services with localstack 
+  # https://docs.localstack.cloud/integrations/terraform/
+  #
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
+  ##########################################################
+
 }
