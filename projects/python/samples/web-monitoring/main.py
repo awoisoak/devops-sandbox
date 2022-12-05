439c0a15fd0fef39ee7d19dcaad9e1f9e8c3b7c3
awoisoak <awoisoak@gmail.com>
2022-12-05 21:48:24 +0900

    restart container if app returns error or container is not reachable

--- projects/python/samples/web-monitoring/main.py
+++ projects/python/samples/web-monitoring/main.py
@@ -4,9 +4,11 @@ import docker
 import requests
 import smtplib
 import os
+import schedule
 from docker.errors import APIError
+from docker.models.containers import Container
 from requests import ConnectionError
-from utils.print_utils import printg, printr
+from utils.print_utils import printg, printr, printy
 
 # https://support.google.com/accounts/answer/185833?hl=en
 DEVOPS_EMAIL_ADDRESS = os.environ.get("DEVOPS_EMAIL_ADDRESS")
@@ -32,7 +34,7 @@ def send_alert(msg):
 def run_container():
     """Run a Photo-shop web server container"""
     try:
-        docker_client.containers.run(
+        return docker_client.containers.run(
             "awoisoak/photo-shop",
             detach=True,
             ports={9000: 9000},
@@ -43,17 +45,42 @@ def run_container():
         send_alert(f"Subject: App could not be started!\nTake a look!")
 
 
-run_container()
+def restart_container():
+    printr("restarting container...")
+    container.restart()
+
+
+def stop_container(msg):
+    printr("stopping container due to {msg}...")
+    container.stop()
+
+
+def monitoring():
+    try:
+        response = requests.get("http://localhost:9000/")
+        if response.status_code == 200:
+            printg("Application is running successfully")
+        else:
+            printr("Application is down")
+            send_alert("Subject: App is down!\nTry restarting the container")
+            restart_container()
+
+    except ConnectionError as msg:
+        printr(f"ConnectionError: {msg}")
+        send_alert("Subject: Could not connect to container!\nCheck if container is actually running")
+        restart_container()
+
+
+container = run_container()
+printy(container)
 printg("Waiting 5s for photo-shop web server to be available...")
 sleep(5)
 
+schedule.every(6).seconds.do(monitoring)
 try:
-    response = requests.get("http://localhost:9000/")
-    if response.status_code == 200:
-        printg("Application is running succesfully")
-    else:
-        printr("Application is down")
-        send_alert("Subject: App is down!\nTry restarting the application, otherwise the container")
-except ConnectionError as msg:
-    printr(f"ConnectionError: {msg}")
-    send_alert("Subject: Could not connect to container!\nCheck if container is actually running")
+    while True:
+        schedule.run_pending()
+except KeyboardInterrupt:
+    stop_container("Keyboard Interrupt detection")
+finally:
+    stop_container("Finally detection")
